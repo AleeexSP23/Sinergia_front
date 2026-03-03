865ed83cdc39cc38a7029e02fcfbfa4c457267a4
Alejandro <a.santos.plato@gmail.com>
2026-03-04 00:33:52 +0100

    conexion registro back

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  nombre = '';
+  correo = '';
+  password = '';
+
+  onSubmit() {
+    this.authService.register({
+      nombre: this.nombre,
+      correo: this.correo,
+      password: this.password
+    })
+    .subscribe({
+      next: (response) => {
+        console.log(response);
+        console.log("Usuario registrado correctamente");
+
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
